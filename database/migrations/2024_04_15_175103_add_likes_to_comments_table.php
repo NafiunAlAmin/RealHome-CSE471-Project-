c31f6ecb6e3290d939c0bf14bac937d57330cc23
Nusaiba Zaman <nusaiba.zaman13@gmail.com>
2024-04-22 23:39:07 +0600

    delete

---
+++ database/migrations/2024_04_15_175103_add_likes_to_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+            $table->integer('likes')->default(0); // Add the 'likes' column with a default value of 0
+        $table->integer('dislikes')->default(0); // Add the 'dis
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+};
